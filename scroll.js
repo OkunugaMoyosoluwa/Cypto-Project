9a1c784d97f993149154170261546258c70abc9e
OkunugaMoyosoluwa <108668877+OkunugaMoyosoluwa@users.noreply.github.com>
2022-09-15 16:14:59 +0100

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,25 @@
+ScrollReveal({
+     reset: true,
+     distance: '15px',
+     duration: 2000,
+     delay: 500,
+     easing: 'ease-in',
+    });
+
+    ScrollReveal().reveal('.save', { delay: 500, origin: 'right' });
+    ScrollReveal().reveal('.text', { delay: 500, origin: 'bottom' });
+    ScrollReveal().reveal('.try', { delay: 500, origin: 'top', interval: 200 });
+    ScrollReveal().reveal('.try span', { delay: 500, origin: 'left' });
+    ScrollReveal().reveal('.hero-img', { delay: 700, origin: 'right'});
+    ScrollReveal().reveal('.second-section div', { delay: 500, origin: 'right', interval: 200 });
+    ScrollReveal().reveal('.two', { delay: 700, origin: 'right' });
+    ScrollReveal().reveal('.third-section-content', { delay: 500, origin: 'bottom'});
+    ScrollReveal().reveal('.park-one', { delay: 500, origin: 'bottom' });
+    ScrollReveal().reveal('.test-one', { delay: 500, origin: 'bottom' });
+    ScrollReveal().reveal('.calculate', { delay: 700, origin: 'bottom' });
+    ScrollReveal().reveal('.trade', { delay: 500, origin: 'top' });
+    ScrollReveal().reveal('.sixth-section h1', { delay: 500, origin: 'bottom' });
+    ScrollReveal().reveal('.flex-one .link', { delay: 500, origin: 'left' });
+    ScrollReveal().reveal('.flex-image', { delay: 500, origin: 'right' });
+    ScrollReveal().reveal('.reverse .link', { delay: 500, origin: 'right' });
+    ScrollReveal().reveal('.reverse .flex-image', { delay: 500, origin: 'left'});
